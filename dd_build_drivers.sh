859078eae4a27de288c71e011ab5d934ad1df2f5
Palii <paliydmn@gmail.com>
2024-02-23 11:45:08 +0200

    Update dd_build_drivers.sh
    new dddvb version is added. refactoring

--- dd_build_drivers.sh
+++ dd_build_drivers.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 ##
-##Script revision: 0.0.13
-REV="0.0.13"
+##Script revision: 0.0.16
+REV="0.0.16"
 ##
 # Check root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root!"
   exit
 fi
+GREEN='\033[32m'
+YELLOW='\033[33m'
+RED='\033[31m'
+NC='\033[0m' # No Color
 #Check Args
 if [[ $# -eq 0 ]]; then
-	echo "No arguments passed. Default valuses applyed:
-		FMODE = 0
-		dddvb version = 0.9.38"
+	echo -e "$GREEN No arguments passed. Default valuses applyed:
+		fmode = 0
+		dddvb version = 0.9.39
+		msi = 1 (enabled) $NC"
 	FMODE=0
-	VERSION=38
+	VERSION=39
 else
 	POSITIONAL_ARGS=()
 	while [[ $# -gt 0 ]]; do
 		case $1 in
 		-v|--version)
 		  VERSION="$2"
-			if [[ $VERSION -gt 38 || $VERSION -lt 37 ]]; then
-				echo -e "Wrong version value: $VERSION. Possible values: 37, 38. See help (-h|--help)"
+			#If version is not indicated, set latest release 0.9.39 version
+		    if [[ $VERSION == ""  ]]; then 
+				VERSION=39 
+		    fi
+			if [[ $VERSION -gt 39 || $VERSION -lt 37 ]]; then
+				echo -e "$RED Wrong version value: $VERSION. Possible values: 37, 38, 39. See help (-h|--help) $NC"
 				exit
 			fi
 		  shift # past argument
@@ -31,7 +40,7 @@ else
 		-f|--fmode)
 		  FMODE="$2"
 		  if [[ $FMODE -gt 3 || $FMODE -lt 0 || $FMODE == "" ]]; then
-			echo -e "FMODE=$FMODE is not correct. See help (-h|--help)"
+			echo -e "$RED FMODE=$FMODE is not correct. See help (-h|--help) $NC"
 			exit
 		  fi
 		  shift # past argument
@@ -44,7 +53,7 @@ else
 		  ;;
 		-h|--help)
 		  HELP=1
-		  echo -e "version $REV"
+		  echo -e "Script version $REV"
 		  echo -e "dddvb_build script [OPTIONS] \n"
 		  echo -e "-f|--fmode \n\t Set FMODE (default with fmode 0)\n\t Possible values: 0,1,2,3"
 		  echo -e "\t Modes for Max S8/SX8/SX8 Basic:
@@ -56,12 +65,13 @@ else
 			\t Quattro - LNB / cascade outputs of multiswitches
 			fmode=3
 			\t Unicable LNB or JESS / Unicabel output of the multiswitch"
-		  echo -e "-v|--version \n\t dddvb version to build \n\t Possible values: 37, 38:
+		  echo -e "-v|--version \n\t dddvb version to build \n\t Possible values: 37, 38, 39:
 			37 is dddvb-0.9.37 
-			38 is dddvb-0.9.38"
+			38 is dddvb-0.9.38
+			39 is dddvb-0.9.39"
 		  echo -e "-g|--git \n\t ignire -v option and build dddvb latest git version\n"
 		  echo -e "-m|--max-adapters \n\t Set MAX ADAPTERS \n\t Possible values: 1,2"
-		  echo -e "\t Max Adapters (Default value for dddvb 0.9.38 is 64):
+		  echo -e "\t Max Adapters (Default value for dddvb 0.9.38/39 is 64):
 			-m 1 = MAX ADAPTERS 128 (set 128 adapters)
 			-m 2 = MAX ADAPTERS 256 (set 256 adapters)\n"
 		  echo -e "-M|--msi \n\t If you have I²C-Timeouts, please disable MSI for ddbridge \n\t Possible values: 0-disable, 1-enable (default)"
@@ -76,7 +86,7 @@ else
 		  elif [[ $MAX_A == 2 ]]; then
 			MAX_A=256
 		  else
-			echo -e "MAX_ADAPTERS=$MAX_A is not correct. See help (-h|--help)"
+			echo -e "$RED MAX_ADAPTERS=$MAX_A is not correct. See help (-h|--help) $NC"
 			exit
 		  fi
 		##
@@ -94,7 +104,7 @@ else
 		  elif [[ $MSI == 0 ]]; then 
 			MSI=0;
 		  else
-			echo -e "MSI=$MSI is not correct. See help (-h|--help)"
+			echo -e "$RED MSI=$MSI is not correct. See help (-h|--help) $NC"
 			exit
 		  fi
 		  echo -e "MSI = $MSI"
@@ -102,7 +112,7 @@ else
 		  shift # past value
 		  ;;
 		-*|--*)
-		  echo "Unknown option $1"
+		  echo "$RED Unknown option $1 $NC"
 		  exit 1
 		  ;;
 		*)
@@ -135,59 +145,38 @@ fi
 #
 if [[ $GIT == 1 ]]; then
 	if [[ ! -z $VERSION ]]; then
-		echo -e "Version value is ignored. See help (-h|--help)\n"
+		echo -e "$YELLOW Version value is ignored. See help (-h|--help) $NC\n"
 	fi
-		echo "Git is selected"
-        echo -e "\x1B[01;93m Download Latest Git Version: \x1B[0m"
-        if [ -d "/usr/src/dddvb-git" ]; then
-            mv /usr/src/dddvb-git /usr/src/dddvb-git.back.$SUFFIX
-        fi
-        git clone https://github.com/DigitalDevices/dddvb.git dddvb-git
-        cd dddvb-git
-        sed -i '/$(MAKE) -C app/s/^/# /' Makefile
-		#MAX Adapters limitation is not appyed for version > 0.9.37
-		DVB_DEV_PWD=$(find . -name 'dvbdev.h')
-		sed -i -e "s/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g" $DVB_DEV_PWD
-		sed -i -e "s/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS $MAX_A/g" $DVB_DEV_PWD
-elif 
-	[[ $VERSION == 37 ]]; then
-	echo -e "\x1B[01;93m Download Release version: 0.9.37 \x1B[0m"
-    if [ -f "/usr/src/0.9.37.tar.gz" ]; then
-        mv /usr/src/0.9.37.tar.gz /usr/src/0.9.37.tar.gz.back.$SUFFIX
+	echo "Git is selected"
+    echo -e "$GREEN Download Latest Git Version: $NC"
+    if [ -d "/usr/src/dddvb-git" ]; then
+		mv /usr/src/dddvb-git /usr/src/dddvb-git.back.$SUFFIX
     fi
-        wget https://github.com/DigitalDevices/dddvb/archive/0.9.37.tar.gz
-        if [ -d "/usr/src/dddvb-0.9.37" ]; then
-			mv /usr/src/dddvb-0.9.37 /usr/src/dddvb-0.9.37.back.$SUFFIX
-        fi
-        tar -xf 0.9.37.tar.gz
-        cd dddvb-0.9.37
-#MAX Adapters limitation is not appyed for version > 0.9.37
-		DVB_DEV_PWD=$(find . -name 'dvbdev.h')
-		sed -i -e "s/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g" $DVB_DEV_PWD
-		sed -i -e "s/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS $MAX_A/g" $DVB_DEV_PWD
-		#sed -i -e 's/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g' dvb-core/dvbdev.h
-        #sed -i -e 's/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS 256/g' dvb-core/dvbdev.h
-elif
-#If version is not indicated, set latest release 0.9.38 version
-	[[ $VERSION == 38 || $VERSION == "" ]]; then
-		VERSION=38
-		echo -e "\x1B[01;93m Download Release version: 0.9.$VERSION \x1B[0m"
-        if [ -f "/usr/src/0.9.$VERSION.tar.gz" ]; then
-            mv /usr/src/0.9.$VERSION.tar.gz /usr/src/0.9.$VERSION.tar.gz.back.$SUFFIX
-        fi
-        wget https://github.com/DigitalDevices/dddvb/archive/0.9.$VERSION.tar.gz
-        if [ -d "/usr/src/dddvb-0.9.$VERSION" ]; then
-            mv /usr/src/dddvb-0.9.$VERSION /usr/src/dddvb-0.9.$VERSION.back.$SUFFIX
-        fi
-        tar -xf 0.9.$VERSION.tar.gz
-        cd dddvb-0.9.$VERSION
-#Do not build /app 		
-		sed -i '/$(MAKE) -C app/s/^/# /' Makefile
-		#MAX Adapters limitation is not appyed for version > 0.9.37
-		DVB_DEV_PWD=$(find . -name 'dvbdev.h')
-		sed -i -e "s/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g" $DVB_DEV_PWD
-		sed -i -e "s/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS $MAX_A/g" $DVB_DEV_PWD
-		#			
+    git clone https://github.com/DigitalDevices/dddvb.git dddvb-git
+    cd dddvb-git
+    sed -i '/$(MAKE) -C app/s/^/# /' Makefile
+	#MAX Adapters limitation is not appyed for version > 0.9.37
+	DVB_DEV_PWD=$(find . -name 'dvbdev.h')
+	sed -i -e "s/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g" $DVB_DEV_PWD
+	sed -i -e "s/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS $MAX_A/g" $DVB_DEV_PWD
+elif [[ $VERSION == 37 || $VERSION == 38 || $VERSION == 39 ]]; then
+	echo -e "$GREEN Download Release version: 0.9.$VERSION $NC"
+    if [ -f "/usr/src/0.9.$VERSION.tar.gz" ]; then
+		mv /usr/src/0.9.$VERSION.tar.gz /usr/src/0.9.$VERSION.tar.gz.back.$SUFFIX
+    fi
+    wget https://github.com/DigitalDevices/dddvb/archive/0.9.$VERSION.tar.gz
+    if [ -d "/usr/src/dddvb-0.9.$VERSION" ]; then
+		mv /usr/src/dddvb-0.9.$VERSION /usr/src/dddvb-0.9.$VERSION.back.$SUFFIX
+    fi
+	tar -xf 0.9.$VERSION.tar.gz
+    cd dddvb-0.9.$VERSION
+#Do not build /app 	folder	
+	sed -i '/$(MAKE) -C app/s/^/# /' Makefile
+	#MAX Adapters limitation is not appyed for version > 0.9.37
+	DVB_DEV_PWD=$(find . -name 'dvbdev.h')
+	sed -i -e "s/^#if defined(CONFIG_DVB_MAX_ADAPTERS).*$/#if 0/g" $DVB_DEV_PWD
+	sed -i -e "s/DVB_MAX_ADAPTERS 64/DVB_MAX_ADAPTERS $MAX_A/g" $DVB_DEV_PWD
+	#			
 fi
 make clean
 #Skip warnings, catch only Errors
@@ -200,15 +189,21 @@ mkdir -p /etc/depmod.d
 echo 'search extra updates built-in' >/etc/depmod.d/extra.conf
 depmod -a
 if [[ -z "${FMODE}" ]]; then
-	echo -e "\x1B[01;93m FMODE is not indicated. Default value = 0 (4 tuner mode) \x1B[0m"
+	echo -e "$YELLOW FMODE is not indicated. Default value = 0 (4 tuner mode) $NC"
 	FMODE=0
 fi
 if [[ -z "${MSI}" ]]; then
-	echo -e "\x1B[01;93m MSI is not indicated. Default value = 1 (enable) \x1B[0m"
+	echo -e "$YELLOW MSI is not indicated. Default value = 1 (enable) $NC"
 	MSI=1;
 fi
-echo "options ddbridge fmode=$FMODE msi=$MSI" | sudo tee /etc/modprobe.d/ddbridge.conf
+#need to check if ddbridge file is exists.
+DDB_CONF_FILE="/etc/modprobe.d/ddbridge.conf"
+if [ ! -f "$DDB_CONF_FILE" ]; then
+	echo -e "$GREEN ddbridge.conf is created. $NC"
+	echo "options ddbridge fmode=$FMODE msi=$MSI" | sudo tee $DDB_CONF_FILE
+fi
 modprobe ddbridge
+cat $DDB_CONF_FILE
 modinfo ddbridge | grep 'version:'
 # start HW/OS details
 . /etc/os-release
@@ -231,8 +226,9 @@ if [ -z "$ERR_MAKE" ] && [ -z "$ERR_MAKE_INST" ]; then
         echo 'Done! Please reboot the server'
 else
 # end HW/OS details
-	echo -e "\x1B[01;93m Errors during drivers building process.\x1B[0m"
-	echo -e "\x1B[01;93m Please copy output and send to the script author: paliydmn@digitaldevices.de\x1B[0m"
-	echo -e "\x1B[01;93m Make ERR: \n $ERR_MAKE\nInstall ERR: \n $ERR_MAKE_INST \x1B[0m"
+	echo -e "$RED Errors during drivers building process.$NC"
+	echo -e "$RED Please copy output and send to the script author: paliydmn@digitaldevices.de $NC"
+	echo -e "$RED Make ERR: \n $ERR_MAKE\nInstall ERR: \n $ERR_MAKE_INST $NC"
+	echo -e "$RED $(uname -a) $NC"
 fi
 
